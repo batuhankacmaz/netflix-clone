0ef94440a6236725b42b4b11e60821e23d00ffc3
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-03-11 23:28:16 +0300

    Create CollectionViewTableCell and Table Header

---
+++ Netflix Clone/Views/CollectionViewTVC.swift
@@ -0,0 +1,57 @@
+//
+//  CollectionViewTVC.swift
+//  Netflix Clone
+//
+//  Created by Batuhan Kacmaz on 11.03.2023.
+//
+
+import UIKit
+
+class CollectionViewTVC: UITableViewCell {
+
+    static let identifier = "CollectionViewTVC"
+    
+    private let collectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+}
+
+
+extension CollectionViewTVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    
+}
